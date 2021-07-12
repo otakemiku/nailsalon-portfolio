1e9a75777c13c1062f5d6a72f89d82e8e83e5fe8
Miku <83892697+otakemiku@users.noreply.github.com>
2021-07-12 19:31:43 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// フェードイン
+
 $(function () {
   $(window).scroll(function () {
     $('.fadein').each(function () {
@@ -10,3 +12,61 @@ $(function () {
     });
   });
 });
+
+
+// １文字ずつ文字出現
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});
+
+
+function BlurTextAnimeControl() {
+	$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+		}else{
+		$(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+		}
+		});
+}
+$(window).scroll(function () {
+	BlurTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
